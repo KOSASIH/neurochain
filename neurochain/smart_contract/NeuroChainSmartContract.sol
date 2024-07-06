6d738e41a3a0a90612f5c23599ed097d116f01cb
KOSASIH <kosasihg88@gmail.com>
2024-07-06 20:42:17 +0700

    Create NeuroChainSmartContract.sol

---
+++ neurochain/smart_contract/NeuroChainSmartContract.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+contract NeuroChainSmartContract {
+    address private owner;
+    mapping (address => uint256) public balances;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+}
