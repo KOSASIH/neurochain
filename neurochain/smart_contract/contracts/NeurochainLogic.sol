3a73ffef9242146f937c6339f707c7459420d490
KOSASIH <kosasihg88@gmail.com>
2024-07-06 20:54:19 +0700

    Create NeurochainLogic.sol

---
+++ neurochain/smart_contract/contracts/NeurochainLogic.sol
@@ -0,0 +1,8 @@
+// neurochain/smart_contract/contracts/NeurochainLogic.sol
+pragma solidity ^0.8.0;
+
+contract NeurochainLogic {
+    function executeTransaction(NeurochainStorage _storage) public {
+        // advanced logic implementation
+    }
+}
