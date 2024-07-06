8107b09603010aacee762071b67689647064d95f
KOSASIH <kosasihg88@gmail.com>
2024-07-06 18:28:39 +0700

    Create security.py

---
+++ neurochain/guard/security.py
@@ -0,0 +1,10 @@
+import cryptography
+
+class Security:
+    def __init__(self, private_key, public_key):
+        self.private_key = private_key
+        self.public_key = public_key
+
+    def encrypt_data(self, data):
+        # Encrypt data using the private key
+        pass
