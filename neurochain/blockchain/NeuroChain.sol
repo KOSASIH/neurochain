3ff8966f9e29a5b67cd0cbfb38753cfbd2ece23f
KOSASIH <kosasihg88@gmail.com>
2024-07-06 19:45:29 +0700

    Create NeuroChain.sol

---
+++ neurochain/blockchain/NeuroChain.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+contract NeuroChain {
+    address private owner;
+    mapping (address => uint256) public balances;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function transfer(address recipient, uint256 amount) public {
+        require(msg.sender == owner, "Only the owner can transfer tokens");
+        balances[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+    }
+
+    function getBalance(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
