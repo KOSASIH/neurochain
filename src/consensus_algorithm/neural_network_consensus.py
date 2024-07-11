5b8acd85694fb8efa2471514541f5d7c0b3bc40e
KOSASIH <kosasihg88@gmail.com>
2024-07-11 19:13:23 +0700

    Create neural_network_consensus.py

---
+++ src/consensus_algorithm/neural_network_consensus.py
@@ -0,0 +1,10 @@
+import numpy as np
+from src.neural_networks.neural_network import NeuralNetwork
+
+class NeuralNetworkConsensus:
+    def __init__(self):
+        self.neural_network = NeuralNetwork()
+
+    def validate_transaction(self, transaction):
+        # Implement neural network consensus logic
+        pass
